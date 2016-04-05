836728e9ca8f30be120ccf67c3edd529772e0dd3
caikeal <546487590@qq.com>
2016-04-05 11:13:28 +0800

    把小喇叭数量统计单独抽出方法

--- website/protected/controllers/v1/NewsController.php
+++ website/protected/controllers/v1/NewsController.php
@@ -1,7 +1,7 @@
 <?php
 class NewsController extends PublicController{
 	public $layout = false;
-	
+	use hornTrait;
 	/**
 	 * 消息列表
 	 * 涉及news和newsrefresh表
@@ -822,18 +822,7 @@ class NewsController extends PublicController{
 		}
 
 		$lists = array();
-		$command = $connection->createCommand("select count(*) as c from broadcasting_log where member_id = ".$user->id." and created_time >= ".strtotime(date('Y-m-01', strtotime(date("Y-m-d")))));
-		$authority = $command->queryAll();
-		$authorityNumber = 30;
-		$is_exist=PromotionManageAttach::model()->find("member_id={$user['id']}");
-		if($is_exist['small_horn_num']){
-			$all_small=$is_exist['small_horn_num'];
-		}else{
-			$all_small=0;
-		}
-		if ($authority) {
-			$authorityNumber = 30 - $authority[0]['c'] + $all_small;
-		}
+		$authorityNumber=$this->getHornNum($user)?$this->getHornNum($user):"0";
 		if ($infoQuery) {
 			$friendIds = array();
 			foreach ($infoQuery as $key => $value) {
