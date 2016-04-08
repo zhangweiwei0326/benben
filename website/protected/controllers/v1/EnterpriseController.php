d5ef143b9ccb03bccc072cbbf9666969730bb565
caikeal <546487590@qq.com>
2016-04-08 18:54:34 +0800

    非虚拟网在掉申请接口是报错的bug，主要由于未传短号

--- website/protected/controllers/v1/EnterpriseController.php
+++ website/protected/controllers/v1/EnterpriseController.php
@@ -1968,18 +1968,20 @@ class EnterpriseController extends PublicController
             die ();
         }
 
-        //判断是否是政企预留号码，是则直接加入
-        $eminfo=EnterpriseMember::model()->find("contact_id={$enterpriseid} and short_phone={$remark_name} and member_id=0");
-        if($eminfo&&$eminfo['member_id']==0){
-            $eminfo->remark_name=$user['nick_name'];
-            $eminfo->remark_name=$user['name'];
-            $eminfo->phone=$user['phone'];
-            $eminfo->member_id=$user['id'];
-            if($eminfo->save()){
-                $result['ret_num'] = 1888;
-                $result['ret_msg'] = '加入成功！';
-                echo json_encode($result);
-                die ();
+        if($remark_name) {
+            //判断是否是政企预留号码，是则直接加入
+            $eminfo = EnterpriseMember::model()->find("contact_id={$enterpriseid} and short_phone={$remark_name} and member_id=0");
+            if ($eminfo && $eminfo['member_id'] == 0) {
+                $eminfo->remark_name = $user['nick_name'];
+                $eminfo->remark_name = $user['name'];
+                $eminfo->phone = $user['phone'];
+                $eminfo->member_id = $user['id'];
+                if ($eminfo->save()) {
+                    $result['ret_num'] = 1888;
+                    $result['ret_msg'] = '加入成功！';
+                    echo json_encode($result);
+                    die ();
+                }
             }
         }
 
